6edc5af75945ab2bbff476941c1d7558f053d595
rgsad <rsad3262@gmail.com>
2025-02-18 23:48:27 +0300

    tasks solution

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,20 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        int sideLenght = cathetusLength * 2 - 1;
 
+        for (int i = 0; i < cathetusLength; i++) {
+            int counter = cathetusLength;
+            for (int j = 0; j < sideLenght; j++) {
+                if (j < cathetusLength - i - 1 || j > cathetusLength + i - 1) {
+                    System.out.print(' ');
+                } else {
+                    System.out.print(counter);
+                }
+                counter = j < cathetusLength - 1 ? counter - 1 : counter + 1;
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
